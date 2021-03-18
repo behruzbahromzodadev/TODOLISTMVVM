d9bc95767bdb3ff6af439d55ed095bf2be2783a0
behruzbahromzodadev <behruzbahromzoda98@gmail.com>
2021-03-18 15:51:31 +0500

    search function was implemented

--- mobile/app/src/main/java/tj/behruz/todolist/data/TaskAdapter.kt
+++ mobile/app/src/main/java/tj/behruz/todolist/data/TaskAdapter.kt
@@ -7,12 +7,12 @@ import tj.behruz.todolist.database.entities.Task
 import tj.behruz.todolist.databinding.TaskItemBinding
 import tj.behruz.todolist.utils.setTaskBackground
 
-class TaskAdapter(private var tasks:MutableList<Task>, private val handler:(Task)->Unit):RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
+class TaskAdapter(private var tasks: MutableList<Task>, private val handler: (Task) -> Unit): RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
 
-    override fun getItemCount()=tasks.size
+    override fun getItemCount() = tasks.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
-        val binding=TaskItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        val binding = TaskItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return TaskViewHolder(binding)
     }
 
@@ -29,19 +29,19 @@ class TaskAdapter(private var tasks:MutableList<Task>, private val handler:(Task
         notifyItemRemoved(position)
     }
 
-    fun dateChanged(newList:MutableList<Task>){
-        tasks=newList
+    fun dateChanged(newList: MutableList<Task>) {
+        tasks = newList
         notifyDataSetChanged()
     }
 
-    inner class TaskViewHolder(private val binding:TaskItemBinding):RecyclerView.ViewHolder(binding.root){
+    inner class TaskViewHolder(private val binding: TaskItemBinding): RecyclerView.ViewHolder(binding.root) {
 
-        fun setTask(task:Task){
-            binding.toolBarTitle.text=task.title
-            binding.description.text=task.description
-            binding.level.text=task.priority
+        fun setTask(task: Task) {
+            binding.toolBarTitle.text = task.title
+            binding.description.text = task.description
+            binding.level.text = task.priority
             binding.levelBackground.setTaskBackground(task.priority)
-
+            binding.root.tag = task
             binding.executePendingBindings()
         }
 
--- mobile/app/src/main/java/tj/behruz/todolist/presenter/TaskFragment.kt
+++ mobile/app/src/main/java/tj/behruz/todolist/presenter/TaskFragment.kt
@@ -5,12 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import tj.behruz.todolist.R
@@ -28,9 +30,14 @@ class TaskFragment: Fragment() {
     private val binding by lazy { TaskFragmentBinding.inflate(layoutInflater) }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View = binding.root
     private val adapter by lazy { TaskAdapter(arrayListOf()) { handler(it) } }
+    private val listOfTask = mutableListOf<Task>()
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
         initTaskList()
+        search()
+
         lifecycleScope.launch {
             viewModel.getAll().collect { updateUI(it) }
         }
@@ -45,18 +52,21 @@ class TaskFragment: Fragment() {
     private fun updateUI(state: State) {
         when (state) {
             is State.Loading -> {
-
+                binding.progressBar.visibility = View.VISIBLE
             }
             is State.Loaded -> {
-
-                adapter.dateChanged(state.tasks!!.toMutableList())
+                binding.progressBar.visibility = View.GONE
+                listOfTask.addAll(state.tasks!!)
+                adapter.dateChanged(state.tasks.toMutableList())
             }
 
             is State.Empty -> {
-
+                binding.progressBar.visibility = View.GONE
+                binding.notFound.visibility = View.VISIBLE
             }
-
-
+            is State.LoadingFailed -> {
+                Snackbar.make(binding.root, "Error", Snackbar.LENGTH_LONG).show()
+            }
         }
     }
 
@@ -66,6 +76,7 @@ class TaskFragment: Fragment() {
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
                 val adapter = binding.taskList.adapter as TaskAdapter
                 adapter.removeAt(viewHolder.adapterPosition)
+                viewModel.delete(viewHolder.itemView.tag as Task)
             }
         }
         val itemTouchHelper = ItemTouchHelper(swipeHandler)
@@ -77,6 +88,15 @@ class TaskFragment: Fragment() {
         val bundle = bundleOf()
         bundle.putParcelable("task", task)
         findNavController().navigate(R.id.action_taskFragment_to_addTaskFragment, bundle)
+    }
+
+    private fun search(){
+        binding.searchEditText.addTextChangedListener { type->
+            if (type.toString().isNotEmpty()){
+                adapter.dateChanged(listOfTask.filter { it.title.contains(type.toString()) }.toMutableList())
+            }
+
+        }
     }
 
 
